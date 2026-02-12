2616e37aa6ef09b2d74c1f899743c220bda483fe
Lakshmi <kadiyalalakshmi35@gmail.com>
2026-02-12 16:21:01 +0530

    Added example1 file

---
+++ example1.c
@@ -0,0 +1,19 @@
+//Program to add two given numbers
+
+#include <stdio.h>
+int main()
+{
+   int num1, num2;
+
+   //Read the numbers from user
+   printf("Enter two numbers: ");
+   scanf("%d %d", &num1, &num2);
+
+   //Add given two numbers and print
+   printf("Sum of given two numbers is = %d\n", num1 + num2);
+
+   return 0;
+}
+   
+   
+   
